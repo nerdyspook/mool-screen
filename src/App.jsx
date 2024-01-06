1d2cc1d8ede5f3fd4c35d28a2e1b223d889bec5a
Susanto <sushantomahato1@gmail.com>
2024-01-06 11:17:40 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-end mx-auto p-4"></div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
